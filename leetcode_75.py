3c4651807a358e73c3d0f0263aa396463045d036
SahilGunjal <sahilsgunjal@gmail.com>
2023-07-14 12:25:17 +0530

    Linked_list_completed

--- leetcode_75.py
+++ leetcode_75.py
@@ -1214,14 +1214,61 @@ class Solution:
 
         return prev
 
+
 """
-Question 32: 2130:Maximum Twin Sum of a Linked List
+Question 32: 2130: Maximum Twin Sum of a Linked List
 In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list is known as the twin of the 
 (n-1-i)th node, if 0 <= i <= (n / 2) - 1. For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the 
 twin of node 2. These are the only nodes with twins for n = 4. The twin sum is defined as the sum of a node and its twin.
 Given the head of a linked list with even length, return the maximum twin sum of the linked list.
 
+Intuition: Reverse the 2nd half of the linkedlist and then traverse both the half's simultaneously check the sum and 
+store the maximum sum return it after the loop ends.
 """
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, node):
+        prev = None
+        curr = node
+        while curr:
+            new = curr.next
+            curr.next = prev
+            prev = curr
+            curr = new
+
+        return prev
+
+    def pairSum(self, head: Optional[ListNode]) -> int:
+
+        if head:
+            forHead = head
+            slow = head
+            fast = head
+
+            while fast.next.next:
+                slow = slow.next
+                fast = fast.next.next
+
+            revHead = self.reverseList(slow.next)
+
+        maxSum = 0
+        while revHead:
+            maxSum = max(maxSum, revHead.val + forHead.val)
+            revHead = revHead.next
+            forHead = forHead.next
+
+        return maxSum
+
+
+"""
+----------------------------------  Binary-Tree-DFS  ----------------------------------
+"""
+
 
 
 
